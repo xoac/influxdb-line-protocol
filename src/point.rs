73e7ca2d15b111ff037de9d5b4a0827baa5c9746
Sylwester Rąpała <sylwesterrapala@outlook.com>
2020-03-12 22:36:14 +0100

    Esier way to create Point

--- src/point.rs
+++ src/point.rs
@@ -1,4 +1,5 @@
 use super::{error::Error, escape, Field, FiledSet, Measurement, Tag, TagSet, Timestamp};
+use std::{convert::TryInto, iter::FromIterator};
 
 /// Represents a single data record
 ///
@@ -68,9 +69,70 @@ impl PointBuilder {
         self
     }
 
+    pub fn add_tags<I>(mut self, tags: I) -> Self
+    where
+        I: IntoIterator,
+        I::Item: Into<Tag>,
+    {
+        self.tag_set = self
+            .tag_set
+            .into_iter()
+            .chain(tags.into_iter().map(|x| x.into()))
+            .collect::<TagSet>();
+        self
+    }
+
+    pub fn try_add_tag<I, E>(self, tag: I) -> Result<Self, E>
+    where
+        I: TryInto<Tag>,
+        E: From<I::Error>,
+    {
+        let tag = tag.try_into()?;
+        Ok(self.add_tag(tag))
+    }
+
+    pub fn try_add_tags<I, E>(self, iter: I) -> Result<Self, E>
+    where
+        I: IntoIterator,
+        I::Item: TryInto<Tag>,
+        E: From<<I::Item as TryInto<Tag>>::Error>,
+    {
+        let tags_iter = iter.into_iter().map(|x| x.try_into());
+        let res_tags = Result::from_iter(tags_iter.collect::<Vec<Result<Tag, _>>>());
+        let v: Vec<Tag> = res_tags?;
+
+        Ok(self.add_tags(v))
+    }
+
     pub fn add_field(mut self, field: Field) -> Self {
         self.field_set.push(field);
         self
+    }
+
+    pub fn add_fields<I>(mut self, fields: I) -> Self
+    where
+        I: IntoIterator,
+        I::Item: Into<Field>,
+    {
+        self.field_set = self
+            .field_set
+            .into_iter()
+            .chain(fields.into_iter().map(|x| x.into()))
+            .collect::<Vec<_>>();
+        self
+    }
+
+    pub fn try_add_fields<I, E>(self, iter: I) -> Result<Self, E>
+    where
+        I: IntoIterator,
+        I::Item: TryInto<Field>,
+        E: From<<I::Item as TryInto<Field>>::Error>,
+    {
+        let tags_iter = iter.into_iter().map(|x| x.try_into());
+        let res_tags = Result::from_iter(tags_iter.collect::<Vec<Result<Field, _>>>());
+        let v: Vec<Field> = res_tags?;
+
+        Ok(self.add_fields(v))
     }
 
     pub fn timestamp(mut self, timestamp: impl Into<Timestamp>) -> Self {
@@ -90,5 +152,31 @@ impl PointBuilder {
             field_set: self.field_set,
             timestamp: self.timestamp,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_vec_of_fields_to_builder() {
+        let a = Field::new("a", "b").unwrap();
+        let b = Field::new("c", 6i64).unwrap();
+
+        let _point = Point::builder("test")
+            .add_fields(vec![a, b])
+            .build()
+            .unwrap();
+    }
+
+    #[test]
+    fn try_add_tags_to_builder() {
+        let v = vec![("field1", "value1"), ("field2", "value2")];
+        let _point = Point::builder("test")
+            .try_add_fields::<_, Error>(v)
+            .unwrap()
+            .build()
+            .unwrap();
     }
 }
