ae01e2203a2ab6e7e6fd65ff5aec95b9cc6eb5d8
Sylwester Rąpała <sylwesterrapala@outlook.com>
2020-03-16 13:06:30 +0100

    Remove trait IntoPoint. It can be replaced by Into<Point>

--- src/lib.rs
+++ src/lib.rs
@@ -22,11 +22,8 @@ mod point;
 pub mod tag;
 mod timestamp;
 
-mod into_point;
-
 pub use batch::Batch;
 pub use field::{Field, FieldValue};
-pub use into_point::IntoPoint;
 pub use measurement::Measurement;
 pub use point::Point;
 pub use tag::Tag;
