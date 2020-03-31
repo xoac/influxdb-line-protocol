269b93aaed35374514d099d042c8601801ee1248
Sylwester Rąpała <sylwesterrapala@outlook.com>
2020-03-31 17:48:23 +0200

    Implement timestamp precision

--- src/timestamp.rs
+++ src/timestamp.rs
@@ -1,7 +1,15 @@
+/// InfluxDB Timestamp
+///
+/// Values are counted from UNIX_EPOCH.
+///
+///[external source](https://v2.docs.influxdata.com/v2.0/write-data/#timestamp-precision)
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Timestamp {
     Now,
     Nanos(i64),
+    Micro(i64),
+    Milli(i64),
+    Secs(i64),
 }
 
 impl Timestamp {
@@ -9,6 +17,31 @@ impl Timestamp {
         match self {
             Self::Now => None,
             Self::Nanos(v) => Some(v),
+            Timestamp::Micro(v) => v.checked_mul(10i64.pow(3)),
+            Timestamp::Milli(v) => v.checked_mul(10i64.pow(6)),
+            Timestamp::Secs(v) => v.checked_mul(10i64.pow(9)),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn timestamp_as_nanos_test() {
+        assert_eq!(Timestamp::Secs(1).timestamp_nanos(), Some(10i64.pow(9)));
+        assert_eq!(
+            Timestamp::Milli(10).timestamp_nanos(),
+            Some(10 * 10i64.pow(6))
+        );
+        assert_eq!(
+            Timestamp::Micro(10).timestamp_nanos(),
+            Some(10 * 10i64.pow(3))
+        );
+        assert_eq!(
+            Timestamp::Nanos(10).timestamp_nanos(),
+            Some(10 * 10i64.pow(0))
+        );
     }
 }
